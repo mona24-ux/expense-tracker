78ca604a9c4556f74bdb5d7d7cc671f37c42c6ae
Momina Shaheen <mominashaheen945@gmail.com>
2026-07-21 19:53:33 -0700

    Created Expense Model and Project Structure

---
+++ lib/models/expense.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+class Expense {
+  final String id;
+  final String title;
+  final double amount;
+  final String category;
+  final DateTime date;
+
+  Expense({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.category,
+    required this.date,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'category': category,
+      'date': date.toIso8601String(),
+    };
+  }
+
+  factory Expense.fromMap(Map<String, dynamic> map) {
+    return Expense(
+      id: map['id'],
+      title: map['title'],
+      amount: map['amount'],
+      category: map['category'],
+      date: DateTime.parse(map['date']),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+  factory Expense.fromJson(String source) => Expense.fromMap(json.decode(source));
+}
